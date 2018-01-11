a8819986c901ac561205f29a8c5159412e490cf5
Simston <stephane.simon51@gmail.com>
2018-01-11 19:15:03 +0100

    Adding TextView for displaying days in history

--- app/src/main/java/fr/simston/moodtracker/Controllers/Activities/HistoricalActivity.java
+++ app/src/main/java/fr/simston/moodtracker/Controllers/Activities/HistoricalActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Display;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -28,6 +29,8 @@ public class HistoricalActivity extends AppCompatActivity {
     private LinearLayout layout6;
     private LinearLayout layout7;
 
+    private TextView tv1, tv2, tv3, tv4, tv5, tv6, tv7;
+
     private int positionOfMood;
 
     private int currentDay;
@@ -40,7 +43,6 @@ public class HistoricalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_historical);
-
 
 
         //get the Intent that started this Activity
@@ -58,7 +60,7 @@ public class HistoricalActivity extends AppCompatActivity {
         initializeViews();
     }
 
-    private void initializeViews(){
+    private void initializeViews() {
         layout1 = findViewById(R.id.linear1);
         layout2 = findViewById(R.id.linear2);
         layout3 = findViewById(R.id.linear3);
@@ -67,76 +69,87 @@ public class HistoricalActivity extends AppCompatActivity {
         layout6 = findViewById(R.id.linear6);
         layout7 = findViewById(R.id.linear7);
 
+        tv1 = findViewById(R.id.tvLinear1);
+        tv2 = findViewById(R.id.tvLinear2);
+        tv3 = findViewById(R.id.tvLinear3);
+        tv4 = findViewById(R.id.tvLinear4);
+        tv5 = findViewById(R.id.tvLinear5);
+        tv6 = findViewById(R.id.tvLinear6);
+        tv7 = findViewById(R.id.tvLinear7);
+
         ArrayList<MoodStock> mMoodStockArrayList = MainActivity.mMoodStockArrayList;
 
         // Retrieving objects in the ArrayList<MoodStock>
         int i = 1;
-        for (MoodStock moodStock: mMoodStockArrayList) {
+        for (MoodStock moodStock : mMoodStockArrayList) {
             positionOfMood = moodStock.getPositionOfMood();
             dayOfMoodInArray = moodStock.getDay();
             monthOfMoodInArray = moodStock.getMonth();
             String forTv = calculForDaysDisplay(dayOfMoodInArray, monthOfMoodInArray);
-            if(i == 1){
+            if (i == 1) {
                 modifParamOfLinearLayout(layout1, positionOfMood);
+                tv1.setText(forTv);
                 Log.e("forTv", forTv);
-            }else if(i ==2){
-                modifParamOfLinearLayout(layout2,positionOfMood);
-                Log.e("forTv", forTv);
+            } else if (i == 2) {
+                modifParamOfLinearLayout(layout2, positionOfMood);
+                tv2.setText(forTv);
 
-            }else if(i ==3){
-                modifParamOfLinearLayout(layout3,positionOfMood);
-                Log.e("forTv", forTv);
+            } else if (i == 3) {
+                modifParamOfLinearLayout(layout3, positionOfMood);
+                tv3.setText(forTv);
 
-            }else if(i ==4){
-                modifParamOfLinearLayout(layout4,positionOfMood);
-                Log.e("forTv", forTv);
+            } else if (i == 4) {
+                modifParamOfLinearLayout(layout4, positionOfMood);
+                tv4.setText(forTv);
 
-            }else if(i ==5){
-                modifParamOfLinearLayout(layout5,positionOfMood);
-                Log.e("forTv", forTv);
+            } else if (i == 5) {
+                modifParamOfLinearLayout(layout5, positionOfMood);
+                tv5.setText(forTv);
 
-            }else if(i == 6){
-                modifParamOfLinearLayout(layout6,positionOfMood);
-                Log.e("forTv", forTv);
+            } else if (i == 6) {
+                modifParamOfLinearLayout(layout6, positionOfMood);
+                tv6.setText(forTv);
 
-            }
-            else if(i == 7){
-                modifParamOfLinearLayout(layout7,positionOfMood);
-                Log.e("forTv", forTv);
+            } else if (i == 7) {
+                modifParamOfLinearLayout(layout7, positionOfMood);
+                tv7.setText(forTv);
 
             }
             i++;
         }
     }
 
-    private String calculForDaysDisplay(int dayOfMoodInArray, int monthOfMoodInArray){
-        String stringForTextView  ="";
+    private String calculForDaysDisplay(int dayOfMoodInArray, int monthOfMoodInArray) {
+        String stringForTextView;
         int dayForMessage;
 
-        if(currentMonth == monthOfMoodInArray){
+        if (currentMonth == monthOfMoodInArray) {
             dayForMessage = currentDay - dayOfMoodInArray;
 
-            if(dayForMessage == 0){
-                stringForTextView = " aujourd'hui";
-            }else if(dayForMessage == 1){
-                stringForTextView = " hier";
-            }else if(dayForMessage == 2){
-                stringForTextView = " avant-hier";
-            }else if(dayForMessage == 7){
-                stringForTextView = " une semaine";
+            if (dayForMessage == 0) {
+                stringForTextView = "Aujourd'hui";
+            } else if (dayForMessage == 1) {
+                stringForTextView = "Hier";
+            } else if (dayForMessage == 2) {
+                stringForTextView = "Avant-hier";
+            } else if (dayForMessage == 7) {
+                stringForTextView = "Il y a une semaine";
+            } else {
+                stringForTextView = "Il y a " + dayForMessage + " jours";
             }
-        }else {
-            stringForTextView = " il y'as plus d'un mois";
+        } else {
+            stringForTextView = "Il y'as plus d'un mois";
         }
         return stringForTextView;
     }
 
     /**
      * Method to change the appearance of LinearLayout according to stored moods
-     * @param layout LinearLayout
+     *
+     * @param layout         LinearLayout
      * @param positionOfMood int
      */
-    private void modifParamOfLinearLayout(LinearLayout layout, int positionOfMood){
+    private void modifParamOfLinearLayout(LinearLayout layout, int positionOfMood) {
 
         // Get the screen width of device
         Display display = getWindowManager().getDefaultDisplay();
@@ -152,44 +165,44 @@ public class HistoricalActivity extends AppCompatActivity {
         LinearLayout.LayoutParams lp;
 
         // Next position record (mood) we change the Layout settings
-        if(positionOfMood == 0){
+        if (positionOfMood == 0) {
 
             // Changing the desired width according to the desired percentage
-            width = width - (width*70/100);
+            width = width - (width * 75 / 100);
 
             // Set the color desired
             layout.setBackgroundColor(getResources().getColor(R.color.faded_red));
 
             // Applying the new parameters to our LinearLayout
-            lp = new LinearLayout.LayoutParams(width,0);
-            lp.weight =1;
+            lp = new LinearLayout.LayoutParams(width, 0);
+            lp.weight = 1;
             layout.setLayoutParams(lp);
 
-        }else if(positionOfMood == 1){
-            width = width - (width*60/100);
+        } else if (positionOfMood == 1) {
+            width = width - (width * 60 / 100);
             layout.setBackgroundColor(getResources().getColor(R.color.warm_grey));
-            lp = new LinearLayout.LayoutParams(width,0);
-            lp.weight =1;
+            lp = new LinearLayout.LayoutParams(width, 0);
+            lp.weight = 1;
             layout.setLayoutParams(lp);
 
-        }else if(positionOfMood == 2){
-            width = width - (width*40/100);
+        } else if (positionOfMood == 2) {
+            width = width - (width * 40 / 100);
             layout.setBackgroundColor(getResources().getColor(R.color.cornflower_blue_65));
-            lp = new LinearLayout.LayoutParams(width,0);
-            lp.weight =1;
+            lp = new LinearLayout.LayoutParams(width, 0);
+            lp.weight = 1;
             layout.setLayoutParams(lp);
 
-        }else if(positionOfMood == 3){
-            width = width - (width*20/100);
+        } else if (positionOfMood == 3) {
+            width = width - (width * 20 / 100);
             layout.setBackgroundColor(getResources().getColor(R.color.light_sage));
-            lp = new LinearLayout.LayoutParams(width,0);
-            lp.weight =1;
+            lp = new LinearLayout.LayoutParams(width, 0);
+            lp.weight = 1;
             layout.setLayoutParams(lp);
 
-        }else if(positionOfMood == 4){
+        } else if (positionOfMood == 4) {
             layout.setBackgroundColor(getResources().getColor(R.color.banana_yellow));
-            lp = new LinearLayout.LayoutParams(width,0);
-            lp.weight =1;
+            lp = new LinearLayout.LayoutParams(width, 0);
+            lp.weight = 1;
             layout.setLayoutParams(lp);
 
         }
